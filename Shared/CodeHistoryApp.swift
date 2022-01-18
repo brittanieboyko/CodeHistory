62dfbcfec6c659779c6d9d9f4f0a2e59b6c9370a
Brittanie <brittanie.boyko@gmail.com>
2022-01-18 17:58:19 -0500

    Initial Commit

---
+++ Shared/CodeHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeHistoryApp.swift
+//  Shared
+//
+//  Created by Brittanie Boyko on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct CodeHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
