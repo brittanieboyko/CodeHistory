a4849b73ac2cc47f2857895faec682678a8fc707
Brittanie <brittanie.boyko@gmail.com>
2022-01-21 22:01:27 -0500

    re-added deleted contentView

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -10,7 +10,16 @@ import SwiftUI
 let mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
 let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
 
-var body: some View {
-    ZStack {}
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            mainColor
+        }
+    }
 }
 
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
