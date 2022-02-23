32e705c25c3b4362d232e053f5052c0f19bcf22e
Brittanie <brittanie.boyko@gmail.com>
2022-02-23 10:49:13 -0500

    added properties to Game struct determining currentQuestion and end of game

--- Shared/Models/Game.swift
+++ Shared/Models/Game.swift
@@ -9,6 +9,24 @@ import Foundation
 
 struct Game {
     private(set) var guesses = [Question: Int]()
+    private(set) var currentQuestionIndex = 0
+    private(set) var isOver = false
     private let questions = Question.allQuestions.shuffled()
     
+    
+    var currentQuestion: Question {
+        questions[currentQuestionIndex]
+    }
+ 
+    mutating func makeGuessForCurrentQuestion(atIndex index: Int) {
+        guesses[currentQuestion] = index
+    }
+    
+    mutating func updateGameStatus() {
+        if currentQuestionIndex < questions.count - 1 {
+            currentQuestionIndex += 1
+        } else {
+            isOver = true
+        }
+    }
 }
