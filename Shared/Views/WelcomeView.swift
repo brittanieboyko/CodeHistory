be1a4a9f4b0c691ef8f7f9c751502668dd81e29d
Brittanie <brittanie.boyko@gmail.com>
2022-03-02 10:19:09 -0500

    created new file for WelcomeView

---
+++ Shared/Views/WelcomeView.swift
@@ -0,0 +1,20 @@
+//
+//  WelcomeView.swift
+//  CodeHistory (iOS)
+//
+//  Created by Brittanie Boyko on 3/2/22.
+//
+
+import SwiftUI
+
+struct WelcomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WelcomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        WelcomeView()
+    }
+}
