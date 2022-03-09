6a208415ed027000a2e222e47d78b60fc83a9f69
Brittanie <brittanie.boyko@gmail.com>
2022-03-09 10:59:07 -0500

    created new SwiftUI view for QuestionView

---
+++ Shared/Views/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  CodeHistory (iOS)
+//
+//  Created by Brittanie Boyko on 3/9/22.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
