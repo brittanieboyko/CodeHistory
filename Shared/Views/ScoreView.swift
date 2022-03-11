d3ed461ef91ad79c7e1018843efa6b5fb94f67c9
Brittanie <brittanie.boyko@gmail.com>
2022-03-10 21:54:25 -0500

    replaced hardcoded text in scoreView with scoreViewModel guess properties

--- Shared/Views/ScoreView.swift
+++ Shared/Views/ScoreView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ScoreView: View {
+    let viewModel: ScoreViewModel
+    
     var body: some View {
         ZStack {
             GameColor.main.ignoresSafeArea()
@@ -15,14 +17,14 @@ struct ScoreView: View {
                 Spacer()
                 Text("Final Score")
                     .font(.body)
-                Text("???%")
+                Text("\(viewModel.percentage)")
                     .font(.system(size: 50))
                     .bold()
                     .padding()
                 Spacer()
                 VStack {
-                    Text("??? ✅")
-                    Text("??? ❌")
+                    Text("\(viewModel.correctGuesses) ✅")
+                    Text("\(viewModel.incorrectGuesses) ❌")
                         .font(.system(size: 30))
                     Spacer()
                     NavigationLink(destination: GameView(), label: {
@@ -39,6 +41,6 @@ struct ScoreView: View {
 
 struct ScoreView_Previews: PreviewProvider {
     static var previews: some View {
-        ScoreView()
+        ScoreView(viewModel: ScoreViewModel(correctGuesses: 8, incorrectGuesses: 2))
     }
 }
