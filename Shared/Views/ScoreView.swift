7ed540f682b06e18a7ab45cda6b432a8e98b897a
Brittanie <brittanie.boyko@gmail.com>
2022-03-10 21:42:58 -0500

    created ui for scoreView

--- Shared/Views/ScoreView.swift
+++ Shared/Views/ScoreView.swift
@@ -9,7 +9,31 @@ import SwiftUI
 
 struct ScoreView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            GameColor.main.ignoresSafeArea()
+            VStack{
+                Spacer()
+                Text("Final Score")
+                    .font(.body)
+                Text("???%")
+                    .font(.system(size: 50))
+                    .bold()
+                    .padding()
+                Spacer()
+                VStack {
+                    Text("??? ✅")
+                    Text("??? ❌")
+                        .font(.system(size: 30))
+                    Spacer()
+                    NavigationLink(destination: GameView(), label: {
+                        BottomTextView(str: "Re-take Quiz")
+                    })
+                }
+                
+            }
+            .foregroundColor(.white)
+            .navigationBarHidden(true)
+        }
     }
 }
 
